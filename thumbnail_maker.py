166a832499194479f03269128a6e71987a2162f0
minwook han <minux4air@gmail.com>
2021-02-03 17:11:34 +0900

    Shared memory와 Lock 을 사용한 다운,리사지징한 파일크기 구하기

--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -6,12 +6,12 @@ from urllib.parse import urlparse
 from urllib.request import urlretrieve
 import PIL
 from PIL import Image
-from threading import Thread 
+from threading import Thread, Lock
 from queue import Queue
 import multiprocessing
 import pdb
 from imglst import IMG_URLS
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG,format ="%(processName)s::%(threadName)s: %(message)s")
 #logging.basicConfig(filename='logfile.log', level=logging.DEBUG)
 
 class ThumbnailMakerService(object):
@@ -20,17 +20,21 @@ class ThumbnailMakerService(object):
         self.input_dir = self.home_dir + os.path.sep + 'incoming'
         self.output_dir = self.home_dir + os.path.sep + 'outgoing'
         self.img_queue = multiprocessing.JoinableQueue()
+        self.dl_size = 0
+        self.resized_size =  multiprocessing.Value('i', 0)
 
-    def download_image(self,dl_que):
+    def download_image(self,dl_que, dl_size_lock):
         while not dl_que.empty():
             try:
                 url = dl_que.get(block=False)
                 img_filename = urlparse(url).path.split('/')[-1]
                 dest_path = self.input_dir + os.path.sep + img_filename
                 urlretrieve(url, dest_path)
+                with dl_size_lock:
+                    self.dl_size += os.path.getsize(dest_path)
                 self.img_queue.put(img_filename)
                 dl_que.task_done()
-                logging.info(f"--- downloaded : {img_filename}")
+                logging.info(f"-{multiprocessing.current_process().pid}:: downloaded : {img_filename}")
 
             except queue.Empty:
                 logging.info("Queue is Empty")
@@ -81,7 +85,11 @@ class ThumbnailMakerService(object):
                     # save the resized image to the output dir with a modified file name 
                     new_filename = os.path.splitext(filename)[0] + \
                         '_' + str(basewidth) + os.path.splitext(filename)[1]
-                    img.save(self.output_dir + os.path.sep + new_filename)
+                    dest_file = self.output_dir + os.path.sep + new_filename
+                    img.save(dest_file)
+
+                    with self.resized_size.get_lock():
+                        self.resized_size.value += os.path.getsize(dest_file)
 
                 os.remove(self.input_dir + os.path.sep + filename)
                 logging.info(f"done resizing:{multiprocessing.current_process().pid}: {self.input_dir + os.path.sep + new_filename}")
@@ -100,33 +108,35 @@ class ThumbnailMakerService(object):
         start = time.perf_counter()
 
         dl_que = Queue()
+        dl_size_lock = Lock()
         for img_url in img_url_list:
             dl_que.put(img_url)
 
         num_dl_threads = 4 #
         for _ in range(num_dl_threads):
-            t = Thread(target=self.download_image, args=(dl_que,))
+            t = Thread(target=self.download_image, args=(dl_que,dl_size_lock))
             t.start()
 
         num_cpu_core = multiprocessing.cpu_count()
 
         time.sleep(2) # 최초 다운로드가 완료되어 img_queue 에 몇개의 자료가 들어 갈 때까지 기다린다.
         for _ in range(num_cpu_core):
-            print("----------- ") # IMG_QUEUE 에 파일이름이 들어가고 PROCESS 가 만들어지는지체크
-            P = MULTIPROCESSING.PROCESS(TARGET=SELF.PERFORM_RESIZING)
-            P.START()
+            print("----") # img_queue 에 파일이름이 들어가고 Process 가 만들어지는지체크
+            p = multiprocessing.Process(target=self.perform_resizing)
+            p.start()
 
 
-        DL_QUE.JOIN() # QUE 에 파일목록 작성 완료 대기
-        # PERFORM_RESIZE 가 POISON PILL: NONE 으로 중지 하므로 프로세스 개수만큼 NONE삽입
+        dl_que.join() # que 에 파일목록 작성 완료 대기
+        # perform_resize 가 Poison pill: None 으로 중지 하므로 프로세스 개수만큼 None삽입
 
-        FOR _ IN RANGE(NUM_CPU_CORE):
-            SELF.IMG_QUEUE.PUT(NONE)
+        for _ in range(num_cpu_core):
+            self.img_queue.put(None)
 
-        END = TIME.PERF_COUNTER()
+        end = time.perf_counter()
 
 
-        LOGGING.INFO("END thumbnail created in {} seconds".format(end - start))
+        logging.info("END thumbnail created in {} seconds".format(end - start))
+        logging.info(f"Total Downloaded File Size {self.dl_size} : Thumbnailed Size:{self.resized_size.value}")
 
 if __name__ == "__main__":
     tt = ThumbnailMakerService()
